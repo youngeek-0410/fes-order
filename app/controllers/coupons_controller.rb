86e0ebe1ff945dda38c5547b367dee46a5109f16
ichigo-dev <ichigo@web-freework.com>
2021-05-04 01:05:44 +0900

    update

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -7,15 +7,9 @@ class CouponsController < ApplicationController
   end
 
   def create
-    coupon = Coupon.new(name: coupon_params[:name], discount: coupon_params[:discount].to_i, user_id: coupon_params[:user_id], expired_at: Time.current.end_of_day)
+    coupon = Coupon.new(name: params[:coupon][:name], discount: params[:coupon][:discount].to_i, user: current_user, expired_at: Time.current.end_of_day)
     coupon.save!
     GameTicket.all.first.destroy!
     redirect_to user_coupons_path(current_user)
-  end
-
-  private
-
-  def coupon_params
-    params.require(:coupon).permit(:name, :discount, :user_id)
   end
 end
