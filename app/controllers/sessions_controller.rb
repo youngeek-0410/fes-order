6ec99f6b1b5fddfa159739df4f82329f8679323c
ichigo-dev <ichigo@web-freework.com>
2021-05-03 15:38:07 +0900

    ログイン失敗時にメールアドレスを保管するように

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,19 +6,20 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      log_in user
+    @user = User.find_by(email: params[:email])
+    if @user && @user.authenticate(params[:password])
+      sign_in @user
       redirect_to root_path
     else
+      @email = params[:email]
       flash.now[:danger] = 'パスワードまたはメールアドレスが間違っています。'
       render 'new'
     end
   end
 
   def destroy
-    log_out
-    redirect_to root_url
+    sign_out
+    redirect_to root_path
   end
 
 end
