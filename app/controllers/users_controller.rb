2cc5296e44ffbd3726c5ed8e4dd43227754df92b
ichigo-dev <ichigo@web-freework.com>
2021-05-03 18:01:31 +0900

    update users#edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to tasks_path
+      redirect_to root_path
     else
       render 'new'
     end
