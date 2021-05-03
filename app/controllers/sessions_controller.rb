846f016358a9ef4b86ebbece28ccc8dc248bee01
tksleep <main@mail.tksleep.dev>
2021-05-03 15:07:30 +0900

    Add sessions view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
-      redirect_to user
+      redirect_to root_path
     else
       flash.now[:danger] = 'パスワードまたはメールアドレスが間違っています。'
       render 'new'
