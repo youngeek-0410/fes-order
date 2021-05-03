07ec77075771c7ed328e0f2b6181cf95096154f6
tksleep <main@mail.tksleep.dev>
2021-05-04 02:59:30 +0900

    Fix to follow reviews

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,13 +4,7 @@
 
 class ProductsController < ApplicationController
   def show
-    if current_user.card.nil?
-      flash[:warning] = 'クレジットカードが登録されていません。'
-      redirect_to edit_user_path(current_user)
-    else
-      @products = Product.where(shop_id: params[:shop_id])
-      @coupons = Coupon.where(user_id: current_user.id)
-      @user = current_user
-    end
+    @product = Product.find(params[:id])
+    @coupons = Coupon.where(user_id: current_user.id)
   end
 end
