90a9b8beaa35908ca9ccb05b272270d2f2103f4c
ichigo-dev <ichigo@web-freework.com>
2021-05-03 22:37:23 +0900

    update coupon.rb

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -4,8 +4,6 @@
 class Coupon < ApplicationRecord
   belongs_to :user
   has_one :receipt
-  validates :user_id, presence: true
   validates :name, presence: true
   validates :discount, presence: true
-  validates :is_used, presence: true
 end
