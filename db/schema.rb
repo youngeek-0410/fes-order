896de95b4d1a3ee69317a32892dfeda73ee62189
ktokey <ikeziri0219@gmail.com>
2021-05-03 18:42:49 +0900

    update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_03_055426) do
+ActiveRecord::Schema.define(version: 2021_05_03_091403) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -97,6 +97,7 @@ ActiveRecord::Schema.define(version: 2021_05_03_055426) do
     t.boolean "is_used", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "charge_id", null: false
     t.index ["coupon_id"], name: "index_receipts_on_coupon_id"
     t.index ["product_id"], name: "index_receipts_on_product_id"
     t.index ["shop_id"], name: "index_receipts_on_shop_id"
