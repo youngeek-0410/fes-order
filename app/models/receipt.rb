321bf9d4d296dff960d3cba8d6d5982568246d45
ichigo-dev <ichigo@web-freework.com>
2021-05-04 02:03:27 +0900

    update receipt.rb

--- app/models/receipt.rb
+++ app/models/receipt.rb
@@ -6,5 +6,10 @@ class Receipt < ApplicationRecord
   belongs_to :shop
   belongs_to :product
   belongs_to :coupon, optional: true
-  has_many :orders
+  has_one :order
+
+  def to_used
+    self.to_used = true
+    self.save!
+  end
 end
