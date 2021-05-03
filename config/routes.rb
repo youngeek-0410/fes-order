df4fc4ba95d41c75db4e754f2d2ab4c93123db2c
ichigo-dev <ichigo@web-freework.com>
2021-05-03 14:14:53 +0900

    update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,25 @@
 # config - routes
 # ==============================================================================
 Rails.application.routes.draw do
+  root to: 'shops#index', as: :root
+
+  get 'sign_in', to: 'sessions#new', as: :new_session
+  post 'sign_in', to: 'sessions#create', as: :session
+  delete 'sign_out', to: 'sessions#destroy', as: :destroy_session
+
+  resources :users, except: :index
+
+  resources :shops, only: [:index, :show] do
+    resources :products, only: :show, controller: :products
+  end
+
+  resources :receipts, only: [:index, :show, :create] do
+    post 'to_used', on: :member
+  end
+
+  resources :game_tickets, only: [:index, :show]
+
+  resources :coupons, only: [:index, :show, :create] do
+    post 'to_used', on: :member
+  end
 end
