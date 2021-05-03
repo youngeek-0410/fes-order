720f1dfcf3f3ee72ccf8db7d89e38f9972f61903
tksleep <main@mail.tksleep.dev>
2021-05-03 16:55:30 +0900

    Fix to follow review

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,6 +38,7 @@ class UsersController < ApplicationController
   end
 
   private
+
   def user_params
     params.require(:user).permit(:family_name, :given_name, :display_name, :email, :password, :password_confirmation)
   end
