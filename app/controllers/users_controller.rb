a53bc4f20bb50283ac443dd36cce40076920465e
tksleep <main@mail.tksleep.dev>
2021-05-03 16:43:33 +0900

    add users controller and page

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,48 @@
+# ==============================================================================
+# app - controller - users
+# ==============================================================================
+class UsersController < ApplicationController
+  before_action :set_user_find, only: [:show, :edit, :update, :destroy]
+
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to tasks_path
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to :root
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:family_name, :given_name, :display_name, :email, :password, :password_confirmation)
+  end
+
+  def set_user_find
+    @user = User.find(params[:id])
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,16 @@ class User < ApplicationRecord
   has_many :game_ticket
   has_many :coupon
 
+  before_save{
+    self.email = email.downcase
+}
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :family_name, presence: true
+  validates :given_name, presence: true
+  validates :display_name, presence: true
+  validates :email, presence: true, uniqueness: {case_sensitive: false}, format: VALID_EMAIL_REGEX
   has_secure_password
+  validates :password, presence: true, length: { minimum: 8 }, allow_nil: true
 end
