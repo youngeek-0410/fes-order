4f17f26a2ed90c80086db0da77ec3fd9d2891d4a
tksleep <main@mail.tksleep.dev>
2021-05-04 10:41:11 +0900

    Add relationship to game_ticket and shop

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -3,6 +3,8 @@
 # ==============================================================================
 class Shop < ApplicationRecord
   has_many :products
+  has_many :coupon
+  has_many :game_ticket
   has_one :receipt
 
   has_secure_password
