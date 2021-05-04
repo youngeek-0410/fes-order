c9801a8c725423a9338ae2901e80a69f36b25a48
tksleep <main@mail.tksleep.dev>
2021-05-04 18:30:56 +0900

    Add shop link to routes

--- config/routes/management_routes.rb
+++ config/routes/management_routes.rb
@@ -3,6 +3,15 @@
 # ==============================================================================
 Rails.application.routes.draw do
   namespace :management do
+    get 'sign_in', to: 'sessions#new', as: :new_session
+    post 'sign_in', to: 'sessions#create', as: :session
+    delete 'sign_out', to: 'sessions#destroy', as: :destroy_session
+
+    resource :shops do
+      resources :receipts, only: [:index, :show, :to_availabled], controller: :receipts do
+        post 'Receipt.availabled', on: :member
+      end
+    end
   end
 end
 
