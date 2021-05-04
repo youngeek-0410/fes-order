e3349859b738cf500ab89a993a3858250f03e395
tksleep <main@mail.tksleep.dev>
2021-05-04 10:39:54 +0900

    Add reference column to game_tickets and coupons

--- db/migrate/20210502165822_create_coupon.rb
+++ db/migrate/20210502165822_create_coupon.rb
@@ -6,6 +6,7 @@ class CreateCoupon < ActiveRecord::Migration[6.0]
       t.integer :discount, null: false
       t.boolean :is_used, null: false, default: false
       t.datetime :expired_at, null: false
+      t.references :shop, null:false, foreign_key: true
       t.timestamps
     end
   end
