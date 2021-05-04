368c74e2b93c4ef67bfa3c08e4ed1c63579748a8
ichigo-dev <ichigo@web-freework.com>
2021-05-05 06:12:36 +0900

    fix migrate

--- db/migrate/20210502155330_create_product.rb
+++ db/migrate/20210502155330_create_product.rb
@@ -3,7 +3,7 @@ class CreateProduct < ActiveRecord::Migration[6.0]
     create_table :products do |t|
       t.references :shop, null: false, foreign_key: true
       t.string :name, null: false
-      t.text :description, null: false, limit: 256
+      t.text :description, limit: 256
       t.integer :price, null: false
       t.integer :price_tax, null: false
       t.timestamps
