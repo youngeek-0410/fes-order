2a3f164eab177c2c1f188d75dd84ad5daa090162
tksleep <main@mail.tksleep.dev>
2021-05-05 01:32:44 +0900

    add receipts view belongs_to management

--- app/controllers/management/receipts_controller.rb
+++ app/controllers/management/receipts_controller.rb
@@ -2,18 +2,24 @@
 # app - controller - management - receipts
 # ==============================================================================
 class Management::ReceiptsController < Management::ApplicationController
+  before_action :set_receipt, only: [:show, :to_availabled]
+
   def index
-    @receipt = Receipt.where(shop: current_shop, is_used: false)
+    @receipts = Receipt.where(shop_id: current_shop.id, is_used: false).order(:id)
   end
 
   def show
-    @receipt = Receipt.where(shop: current_shop).find(param[:id])
   end
   
   def to_availabled
-    receipt = Receipt.where(shop: current_shop).find(params[:id])
-    receipt.to_availabled
-    flash.now[:info] = "#{receipt.product.name}が受け取り可能になりました。"
+    @receipt.to_availabled
+    flash[:info] = "#{@receipt.product.name}が受け取り可能になりました。"
     redirect_to management_shop_receipts_path
+  end
+
+  private
+
+  def set_receipt
+    @receipt = Receipt.where(shop: current_shop).find(params[:id])
   end
 end
