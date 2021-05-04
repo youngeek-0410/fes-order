b66f481b986e688c7ecc5f7bfe3ba8135b6b3f30
tksleep <main@mail.tksleep.dev>
2021-05-04 22:33:38 +0900

    Fix indent size from 4 to 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,16 @@
 # app - controller - application
 # ==============================================================================
 class ApplicationController < ActionController::Base
-    include SessionsHelper
-    include PayjpHelper
-    include Pagy::Backend
+  include SessionsHelper
+  include PayjpHelper
+  include Pagy::Backend
 
-    protect_from_forgery with: :exception
-    layout 'application'
+  protect_from_forgery with: :exception
+  layout 'application'
 
-    before_action :authenticate!
+  before_action :authenticate!
 
-    def authenticate!
-      redirect_to new_session_path unless authenticated?
-    end
+  def authenticate!
+    redirect_to new_session_path unless authenticated?
   end
+end
