0867e9a73f37640219af8d89143fd1e3ffe7b878
ichigo-dev <ichigo@web-freework.com>
2021-05-04 15:22:18 +0900

    fix

--- app/controllers/receipts_controller.rb
+++ app/controllers/receipts_controller.rb
@@ -32,7 +32,7 @@ class ReceiptsController < ApplicationController
 
     receipt = Receipt.create!(receipts_params)
     GameTicket.create!(user: current_user, expired_at: Time.current.end_of_day, shop_id: receipt.shop_id)
-    redirect_to user_receipt_path(current_user, receipt)
+    redirect_to user_receipts_path
   end
 
   def to_used
