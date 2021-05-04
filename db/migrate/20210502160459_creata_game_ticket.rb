86e28d9c6e337bb525e6cbe9149dae39258c2477
tksleep <main@mail.tksleep.dev>
2021-05-04 12:54:39 +0900

    Add expired_at column to game_tickets table

--- db/migrate/20210502160459_creata_game_ticket.rb
+++ db/migrate/20210502160459_creata_game_ticket.rb
@@ -3,6 +3,7 @@ class CreataGameTicket < ActiveRecord::Migration[6.0]
     create_table :game_tickets do |t|
       t.references :user, null: false, foreign_key: true
       t.boolean :is_used, null: false, default: false
+      t.datetime :expired_at, null: false
       t.references :shop, null:false, foreign_key: true
       t.timestamps
     end
