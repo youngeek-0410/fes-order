f30a5618283aa00238f411291c47fa8ac6798b8b
tksleep <main@mail.tksleep.dev>
2021-05-04 22:32:38 +0900

    Add management rootes

--- config/routes/management_routes.rb
+++ config/routes/management_routes.rb
@@ -3,13 +3,14 @@
 # ==============================================================================
 Rails.application.routes.draw do
   namespace :management do
+    root to: 'shops#show', as: :root
     get 'sign_in', to: 'sessions#new', as: :new_session
     post 'sign_in', to: 'sessions#create', as: :session
     delete 'sign_out', to: 'sessions#destroy', as: :destroy_session
 
-    resource :shops do
-      resources :receipts, only: [:index, :show, :to_availabled], controller: :receipts do
-        post 'Receipt.availabled', on: :member
+    resource :shop do
+      resources :receipts, only: [:index, :show], controller: :receipts do
+        post 'availabled', on: :member
       end
     end
   end
