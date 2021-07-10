a74d0e98892223e12b9d54987e685f9136d5e125
Dz0526 <shining.sun0526@gmail.com>
2021-07-11 00:28:59 +0900

    feat(management/helper): be simple and use matcher

--- spec/helpers/management/sessions_helper_spec.rb
+++ spec/helpers/management/sessions_helper_spec.rb
@@ -2,37 +2,57 @@ require 'rails_helper'
 
 RSpec.describe Management::SessionsHelper do 
   include Management::SessionsHelper
-  context 'sign_in' do
-    before do
-      @shop = create(:shop)    
-      session[:shop_id] = @shop.id
-    end
 
-    it 'sign_in' do
-      sign_in(@shop)
-      expect(session[:shop_id]).to eq(@shop.id)
-    end
+  let(:shop) { create(:shop) }
 
-    it 'display sign_in shop' do
-      expect(current_shop.id).to eq(@shop.id)
-    end
-
-    it 'authenticated!' do
-      expect(authenticated?).to eq(true)
-    end
-
-    it 'sign_out' do
-      expect(sign_out).to eq(nil)
+  describe 'sign_in' do
+    context 'when sign_in' do
+      it 'set session[:shop_id] = shop.id' do
+        sign_in(shop)
+        expect(session[:shop_id]).to eq(shop.id)
+      end
     end
   end
 
-  context 'not sign_in' do
-    it 'do not display sign_in shop' do
-      expect(current_shop).to eq(nil)
+  describe 'current_shop' do
+    subject { current_shop }
+
+    context 'call current_shop with sign_in' do
+      it 'return sign in shop' do
+        sign_in(shop)
+        is_expected.to eq shop
+      end
     end
 
-    it 'not authenticated' do
-      expect(authenticated?).to eq(false)
+    context 'call current_shop without sign_in' do
+      it 'return nil' do
+        is_expected.to be_nil
+      end
+    end
+  end
+
+  describe 'authenticated' do
+    subject { authenticated? }
+
+    context 'call authenticated with sign_in' do
+      it 'return true' do
+        sign_in(shop)
+        is_expected.to be_truthy
+      end
+    end
+
+    context 'call authenticated without sign_in' do
+      it 'return false' do
+        is_expected.to be_falsey
+      end
+    end
+  end
+  
+  describe 'sign_out' do
+    context 'call sign_out' do
+      it 'set session[:shop_id] = nil' do
+        expect(sign_out).to be_nil
+      end
     end
   end
 end
