f798ab30b94b0f66b651360ee444a373fd082e6c
Dz0526 <shining.sun0526@gmail.com>
2021-07-11 00:16:48 +0900

    refactor(helper&factory): be simple and use matchers (let, subject etc)

--- spec/helpers/sessions_helper_spec.rb
+++ spec/helpers/sessions_helper_spec.rb
@@ -2,37 +2,57 @@ require 'rails_helper'
 
 RSpec.describe SessionsHelper do 
   include SessionsHelper
-  context 'sign_in' do
-    before do
-      @user = create(:user)    
-      session[:user_id] = @user.id
-    end
 
-    it 'sign_in' do
-      sign_in(@user)
-      expect(session[:user_id]).to eq(@user.id)
-    end
+  let(:user) { create(:user) }
 
-    it 'display sign_in user' do
-      expect(current_user.id).to eq(@user.id)
-    end
-
-    it 'authenticated!' do
-      expect(authenticated?).to eq(true)
-    end
-
-    it 'sign_out' do
-      expect(sign_out).to eq(nil)
+  describe 'sign_in' do
+    context 'when sign_in' do
+      it 'set session[:user_id] = user.id' do
+        sign_in(user)
+        expect(session[:user_id]).to eq(user.id)
+      end
     end
   end
 
-  context 'not sign_in' do
-    it 'do not display sign_in user' do
-      expect(current_user).to eq(nil)
+  describe 'current_user' do
+    subject { current_user }
+
+    context 'call current_user with sign_in' do
+      it 'return sign in user' do
+        sign_in(user)
+        is_expected.to eq user
+      end
     end
 
-    it 'not authenticated' do
-      expect(authenticated?).to eq(false)
+    context 'call current_user without sign_in' do
+      it 'return nil' do
+        is_expected.to be_nil
+      end
+    end
+  end
+
+  describe 'authenticated' do
+    subject { authenticated? }
+
+    context 'call authenticated with sign_in' do
+      it 'return true' do
+        sign_in(user)
+        is_expected.to be_truthy
+      end
+    end
+
+    context 'call authenticated without sign_in' do
+      it 'return false' do
+        is_expected.to be_falsey
+      end
+    end
+  end
+  
+  describe 'sign_out' do
+    context 'call sign_out' do
+      it 'set session[:user_id] = nil' do
+        expect(sign_out).to be_nil
+      end
     end
   end
 end
